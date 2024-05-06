381c9c0b7614d174db85ad067e90400f311f09df
Gustavo Godoy <93145577+GustavoCGodoy@users.noreply.github.com>
2024-05-06 14:48:54 -0300

    UPLOAD CODIGOS DE SIMULAÇÃO
    - Upload dos códigos de blackjack e roleta

---
+++ blackjack.py
@@ -0,0 +1,134 @@
+import numpy as np
+import plotly.express as px;
+import pandas as pd;
+
+## Classe de jogadores
+class Jogador():
+    mao = []
+    total = 0
+    mao_suave = False
+
+##Classe do dealer
+class Dealer():
+    mao = []
+    carta_principal = ''
+    dinheiro = 0
+
+#Inicialização do deck - 6 baralhos de 52 cartas
+baralho = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4 * 6
+valores_cartas = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+
+##Inicialização de variáveis
+jogadores = list()
+jogadas = 0
+vitorias = 0
+empates = 0
+lucro = []
+rodadas = []
+
+##Jogador compra carta
+def hit(jogador):
+    jogador.mao.append(baralho.pop())
+
+##Embaralhar o deck
+def embaralhar():
+    np.random.shuffle(baralho)
+
+##Distribui cartas para todos os jogadores e o dealer
+def dar_cartas():
+    embaralhar()
+    for jogador in jogadores:
+        jogador.mao.append(baralho.pop())
+        jogador.mao.append(baralho.pop())
+        jogador.mao_suave = isMao_suave(jogador.mao)
+    dealer.mao.append(baralho.pop())
+    dealer.carta_principal = dealer.mao[0]
+    dealer.mao.append(baralho.pop())
+
+##Calcula o valor de uma mão
+def calcular_total(mao):
+    total = sum(valores_cartas[carta] for carta in mao)
+    for carta in mao:
+        if carta == "A" and total>21:
+            total -= 10
+    return total
+
+##Define se uma mão é suave ou não
+def isMao_suave(mao):
+    if calcular_total(mao)==17:
+        for carta in mao:
+            if carta=='A':
+                return True
+    return False
+
+##Esvazia uma mão
+def esvaziar_mao(mao):
+    for carta in mao:
+        baralho.append(carta)
+    nova_mao = []
+    return nova_mao
+    
+##Define o resultado de cada mão dos jogadores em relação ao dealer
+def resultado():
+    global vitorias, empates, jogadas
+    for jogador in jogadores:
+        if calcular_total(jogador.mao)<=21 and calcular_total(jogador.mao)>calcular_total(dealer.mao):
+            dealer.dinheiro -= 30
+            vitorias += 1
+        elif calcular_total(jogador.mao)<=21 and calcular_total(jogador.mao)==calcular_total(dealer.mao):
+            dealer.dinheiro -= 20
+            empates += 1
+        elif calcular_total(dealer.mao)>21 and calcular_total(jogador.mao)<=21:
+            dealer.dinheiro -= 30
+            vitorias += 1
+        jogadas += 1
+        jogador.mao = esvaziar_mao(jogador.mao)
+    dealer.mao = esvaziar_mao(dealer.mao)
+
+##Coleta aposta do jogador
+def coletar_aposta():
+    dealer.dinheiro += 20
+
+##Define qual jogada o jogador vai fazer
+def fazer_jogada(jogador):
+    while jogador.mao_suave and calcular_total(jogador.mao)<18:
+        hit(jogador)
+    if dealer.carta_principal == '2' or dealer.carta_principal == '3':
+        while calcular_total(jogador.mao)<13:
+            hit(jogador)
+    if dealer.carta_principal == '4' or dealer.carta_principal == '5' or dealer.carta_principal == '6':
+        while calcular_total(jogador.mao)<12:
+            hit(jogador)
+    if dealer.carta_principal == '7' or dealer.carta_principal == '8' or dealer.carta_principal == '9'  or dealer.carta_principal == '10' or dealer.carta_principal == 'A':
+        while calcular_total(jogador.mao)<17:
+            hit(jogador) 
+
+
+##Iteração
+for jogador in range(7):
+    jogadores.append(Jogador())
+dealer = Dealer()
+
+qtd_rodadas = int(input("QUANTAS RODADAS A MESA VAI JOGAR?: "))
+
+for rodada in range(qtd_rodadas):
+    dar_cartas()
+    for jogador in jogadores:
+        coletar_aposta()
+        fazer_jogada(jogador)
+    while calcular_total(dealer.mao)<=17:
+        hit(dealer)
+    resultado()
+    lucro.append(dealer.dinheiro)
+    rodadas.append(rodada)
+
+##Print de resultados
+print("Número de jogadas: {}".format(jogadas))
+print("Número de vitórias: {}".format(vitorias))
+print("Número de empates: {}".format(empates))
+print("Taxa de vitória: {:.3f}".format((vitorias/jogadas)*100))
+
+dict = {"lucro": lucro, "rodadas": rodadas}
+df = pd.DataFrame(dict)
+fig = px.line(df, x = "rodadas", y = "lucro", title="Lucro do dealer ao longo do tempo (jogada otimizada)", markers = True)
+fig.show()
--- roleta.py
+++ roleta.py
@@ -1,42 +1,85 @@
-import numpy;
+import numpy as np;
 import plotly.express as px;
 import pandas as pd;
 
+## Classe dos jogadores
 class Jogador:
-    def __init__(self, dinheiro, aposta):
-        self.dinheiro = dinheiro;
-        self.aposta = aposta;
-    historico = [];
-    rodadas = [];
-## njogadores = input("Insira o número de jogadores: ");
+    def __init__(self, dinheiro):
+        self.dinheiro = dinheiro
+    aposta = 0
+    historico = []
+    rodadas = []
 
-objs = list();
-for i in range(10):
-    objs.append(Jogador(1000,0));
-apostas = 0;
-vitorias = 0;
-historicogeral = [];
-group = [];
-rodadasgeral = [];
-for i in range(50):
-    sorteado = numpy.random.randint(0,37)
+def apostar(jogador, rodada):
+    global lucro_cassino
+    global saldoacumulado
+    if jogador.dinheiro<=50:
+        apostado = jogador.dinheiro
+    else:
+        apostado = np.random.randint(20,51)
+    jogador.dinheiro -= apostado
+    lucro_cassino += apostado
+    saldoacumulado -= apostado
+    return apostado
+    
+## Input dos dados que o usuário quer simular
+qtd_jogadores = int(input("Quantos jogadores participarão da simulação?: "))
+qtd_dinheiro = int(input("Qual será a quantia de dinheiro inicial de cada jogador?: "))
+qtd_rodadas = int(input("Quantas rodadas serão simuladas?: "))
+
+##Concatenando todos os jogadores em uma lista
+jogadores = list()
+for i in range(qtd_jogadores):
+    jogadores.append(Jogador(qtd_dinheiro))
+
+## Inicializando variáveis
+apostas = 0
+lucro_cassino = 0
+vitorias = 0
+qtd_prejuizos = 0
+qtd_falencia = 0
+saldo = []
+rodadas = []
+saldoacumulado = qtd_dinheiro*qtd_jogadores
+
+##Simulação das rodadas
+saldo.append(saldoacumulado)
+rodadas.append(0)
+rodada = 1
+for i in range(qtd_rodadas):
+    sorteado = np.random.randint(0,37)
     print("Na rodada ",i+1," o número sorteado foi: ",sorteado)
-    j = 0;
-    for jogador in objs:
-        historicogeral.append(jogador.dinheiro);
-        rodadasgeral.append(i);
-        group.append(j);
+    for jogador in jogadores:
         if jogador.dinheiro>0:
-            jogador.dinheiro -= 20
+            apostado = apostar(jogador, rodada)
             apostas += 1
-            jogador.aposta = numpy.random.randint(0,37)
+            jogador.aposta = np.random.randint(0,37)
             if sorteado == jogador.aposta:
-                jogador.dinheiro += 700
+                jogador.dinheiro += apostado*35
+                lucro_cassino -= apostado*35
+                saldoacumulado += apostado*35
                 vitorias += 1
-        j+=1;
-print("HOUVERAM ",apostas," APOSTAS, COM APENAS ",vitorias,"VITORIAS");
-for jogador in objs:
-    print("O DINHEIRO DO JOGADOR É ", jogador.dinheiro);
+    saldo.append(saldoacumulado)
+    rodadas.append(rodada)
+    rodada += 1
 
-fig = px.line(x = rodadasgeral, y = historicogeral, color = group);
+#Contagem de prejuizos e saldos zerados
+for jogador in jogadores:
+    if jogador.dinheiro < qtd_dinheiro:
+        qtd_prejuizos += 1
+    if jogador.dinheiro == 0:
+        qtd_falencia += 1
+    print(jogador.dinheiro)
+##Impressão dos resultados
+taxa_vitoria = "{:.3f}".format((vitorias/apostas)*100)
+print("HOUVERAM ",apostas," APOSTAS, COM APENAS ",vitorias,"VITORIAS.")
+print("TAXA DE VITÓRIA: ", taxa_vitoria,"%")
+print("DOS {} JOGADORES, {} TERMINARAM O JOGO COM PREJUIZO.".format(qtd_jogadores,qtd_prejuizos))
+print("DESTES, {} JOGADORES TERMINARAM O JOGO COM SALDO ZERADO.".format(qtd_falencia))
+print("O CASSINO LUCROU R$", lucro_cassino, " NO TOTAL")
+
+#Plotagem do gráfico usando Plotly
+dict = {"rodadas": rodadas, "saldo": saldo}
+df = pd.DataFrame(dict)
+fig = px.line(df, x = "rodadas", y = "saldo", title="Saldo dos jogadores ao longo do tempo", markers = True)
 fig.show()
